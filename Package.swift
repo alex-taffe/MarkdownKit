c8440b4f18ffdb1ae35b7d28d462cfe9750ade67
Alex Taffe <alex.taffe@gmail.com>
2022-01-18 12:07:38 -0500

    Add dynamic library to SPM
    Without a dynamic library, Xcode will link statically. This prevents 3rd party frameworks from properly being able to properly link against the framework

--- Package.swift
+++ Package.swift
@@ -14,6 +14,11 @@ let package = Package(
         .library(
             name: "MarkdownKit",
             targets: ["MarkdownKit"]
+        ),
+        .library(
+            name: "MarkdownKitDynamic",
+            type: .dynamic,
+            targets: ["MarkdownKit"]
         )
     ],
     targets: [
